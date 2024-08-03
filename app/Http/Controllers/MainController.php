68251f1f375ae70449503559cf34a693cd085988
Ziaul Kamal <codepad@usa.com>
2024-08-03 18:53:44 +0700

    update Enhance

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -20,23 +20,78 @@ class MainController extends Controller
         $response = Http::get($url);
         $data = $response->json();
 
-        $suggestions = [];
-        if (isset($data['items'])) {
-            foreach ($data['items'] as $item) {
-                $suggestions[] = $item['title'];
+        // Periksa apakah ada kesalahan
+        if ($response->failed()) {
+            $decoded = $response->json();
+            if (isset($decoded['error']['code']) && $decoded['error']['code'] == 429) {
+                $query = urlencode($any);
+                $url = "https://www.google.com/search?q=$query";
+
+                $goutteClient = new GoutteClient();
+                $crawler = $goutteClient->request('GET', $url, [
+                    'headers' => [
+                        'User-Agent' => 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.6478.127 Safari/537.36',
+                        'Accept-Language' => 'en-US',
+                        'Accept' => 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+                    ]
+                ]);
+
+                $searchResults = [];
+
+                $crawler->filter('h3')->each(function ($node) use (&$searchResults) {
+                    $title = $node->text();
+                    $linkElement = $node->closest('a');
+                    $link = $linkElement ? $linkElement->attr('href') : null;
+
+                    if (!empty($title)) {
+                        $searchResults[] = [
+                            'title' => $title,
+                            'link' => $link,
+                        ];
+                    }
+                });
+
+                $suggestions = [];
+
+                if ($searchResults) {
+                    foreach ($searchResults as $item) {
+                        // dd($item);
+                        if (isset($item['title'])) {
+                            $suggestions[] = $item['title'];
+                        }
+                    }
+                }
+
+                // Simpan keyword ke dalam database
+                foreach ($suggestions as $setDatas) {
+                    // dd($setDatas);
+                    $keywordData = KeywordData::firstOrCreate(['keyword' => $setDatas]);
+                    $keywordData->increment('hit');
+                }
+            } else {
+                $suggestions = [];
+                if (isset($data['items'])) {
+                    foreach ($data['items'] as $item) {
+                        if (isset($item['title'])) {
+                            $suggestions[] = $item['title'];
+                        }
+                    }
+                }
+
+                // Simpan keyword ke dalam database
+                foreach ($suggestions as $suggestion) {
+                    $keywordData = KeywordData::firstOrCreate(['keyword' => $suggestion]);
+                    $keywordData->increment('hit');
+                }
             }
         }
 
-        // Simpan keyword ke dalam database
-        foreach ($suggestions as $suggestion) {
-            $keywordData = KeywordData::firstOrCreate(['keyword' => $suggestion]);
-            $keywordData->increment('hit');
-        }
 
         // Pilih salah satu keyword secara acak
+        $popular = [];
         if (count($suggestions) > 0) {
             $randomKeyword = $suggestions[array_rand($suggestions)];
-            $this->searchKeyword($randomKeyword);
+            $popular = $this->searchKeyword($randomKeyword);
         }
 
         // Ambil hingga 100 keyword dari database
@@ -53,6 +108,7 @@ class MainController extends Controller
             'keywords' => $keywordsString,
             'metaKeywords' => $metaKeywords,
             'metaDescription' => $metaDescription,
+            'popular' => $popular,
         ]);
     }
 
@@ -85,7 +141,6 @@ class MainController extends Controller
             }
         });
 
-        return response()->json($searchResults);
+        return $searchResults;
     }
-
 }
